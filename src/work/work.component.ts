70352df7cda847c437d6aa372a5b86b54224eace
ankitcoder10 <59387253+ankitcoder10@users.noreply.github.com>
2021-01-08 01:08:09 +0530

    Update work.component.ts

--- src/work/work.component.ts
+++ src/work/work.component.ts
@@ -2,7 +2,7 @@ import { Component, VERSION } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { MatDialog } from "@angular/material/dialog";
 import { ProjectComponent } from 'src/project/project.component';
-
+import { GraphicpopupComponent } from 'src/graphicpopup/graphicpopup.component';
 
 @Component({
   templateUrl: './work.component.html',
@@ -16,6 +16,13 @@ export class WorkComponent  {
     this.dialog.open(ProjectComponent,{
       width: "800px",
       height: "500px"
+    });
+  }
+  openDialogCarousel(name : string) {
+    this.dialog.open(GraphicpopupComponent,{
+      width: "800px",
+      height: "500px",
+      data: name
     });
   }
 }
