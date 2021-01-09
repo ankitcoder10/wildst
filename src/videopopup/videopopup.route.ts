43351382777d2a2480f50de914309d2d50d674cf
ankitcoder10 <59387253+ankitcoder10@users.noreply.github.com>
2021-01-09 23:53:26 +0530

    Create videopopup.route.ts

---
+++ src/videopopup/videopopup.route.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';  
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { VideopopupComponent }  from '../videopopup/videopopup.component'; 
+  
+export const videopopuptRoutes: Routes = [  
+  { path: 'videopopup', component: VideopopupComponent }  
+];  
